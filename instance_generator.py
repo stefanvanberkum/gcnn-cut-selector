973f0ffb202ad3f28855cb9f729800eaa4d57fbc
stefanvanberkum <40003032+stefanvanberkum@users.noreply.github.com>
2022-06-20 18:13:43 +0200

    Updated code.

--- instance_generator.py
+++ instance_generator.py
@@ -558,11 +558,12 @@ def generate_capfac(n_customers: int, n_facilities: int, filepath: str, rng: np.
     """Generate a capacitated facility location problem instance and writes it to a CPLEX LP file.
 
     This method randomly generates costs, capacities, demands, based on the algorithm described in [1]_. For this
-    purpose, we first randomly place facilities and customers on a 1x1 surface and multiply the Euclidian distance
-    between them by 10 to obtain unit cost of serving a customer from a particular facility. Then, demands are generated
-    from :math:`U(5, 35)`. Capacities :math:`s_j` are generated from :math:`U(10, 160)` and the fixed costs are
-    using the formula :math:`U(0, 90) + U(100, 110) \\cdot \\sqrt{s_j}`, to reflect economies of scale. Finally,
-    capacities are scaled to the desired capacity to demand ratio.
+    purpose, we first randomly place facilities and customers on a 1x1 surface. Then, demands are generated from
+    :math:`U(5, 35)`. Capacities :math:`s_j` are generated from :math:`U(10, 160)` and the fixed costs are using the
+    formula :math:`U(0, 90) + U(100, 110) \\cdot \\sqrt{s_j}`, to reflect economies of scale. The capacities are then
+    scaled to the desired capacity to demand ratio. Finally, we multiply the Euclidian distance between each customer
+    and facility by 10 to obtain the unit cost of serving a customer from a particular facility, which is then
+    multiplied by the demand of that customer to obtain the total transportation costs.
 
     References
     ==========
